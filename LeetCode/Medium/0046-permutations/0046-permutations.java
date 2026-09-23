0c44239abf704585f31b4e620d03467039e5639d
Amvi3007 <amvi3007@gmail.com>
2026-09-23 11:42:20 +0530

    2 ms (36.16%) 45.3 MB (78.76%) 09-23-2026 0046-permutations Medium .java Array

---
+++ LeetCode/Medium/0046-permutations/0046-permutations.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static List<List<Integer>> give(List<Integer> cur,List<Integer> rem, int n,List<List<Integer>> output){
+        if(cur.size() == n){
+            output.add(new ArrayList<>(cur));
+            return output;
+        }
+        for(int i=0;i<rem.size();i++){
+            int x = rem.get(i);
+            cur.add(rem.get(i));
+            rem.remove(i);
+            give(cur,rem,n,output);
+            cur.remove(cur.size()-1);
+            rem.add(i,x);
+        }
+
+        return output;
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> out = new ArrayList<>();
+        List<Integer> cur = new ArrayList<>();
+        List<Integer> rem = new ArrayList<>();
+        for(int x : nums){
+            rem.add(x);
+        }
+        List<List<Integer>> result = give(cur,rem,nums.length,out);
+        return result;
+    }
+}
